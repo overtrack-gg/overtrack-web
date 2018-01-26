02656012b9274b847d920e3d6211b89f5a7e788e
Simon Pinfold <simon@uint8.me>
2018-01-27 12:55:57 +1300

    Update FAQ
    - Add image for 'script wrapper' error
    - Add info about screen overlays causing issues
    - Add info about non-English chars in username
    - Add OBS section
    - Add battletag reading section
    - Add OBS recommendation to relevant sections
    - Add stuttering section recommending OBS method
    - Add battletag reading suggestion to unknown names

--- src/faq/faq.component.ts
+++ src/faq/faq.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
     selector: 'faq',
     templateUrl: './faq.component.html',
 })
-export class FAQComponent {
+export class FAQComponent implements OnInit {
+
+    ngOnInit(): void {
+        $('.scrollto').on('click', e => {
+            $('html, body').animate({scrollTop: $(e.target.hash).offset().top - 60}, 250);
+            return false;
+        });
+        if (document.location.hash){
+            window.setTimeout(e => {$('html, body').animate({scrollTop: $(document.location.hash).offset().top - 60}, 250);}, 0);
+        }
+    }
 }
